bb66e681d0da9937894e6401f3f81a2de4c524d5
Mvheem <mvheem2@gmail.com>
2020-10-07 18:58:54 +0200

    UI vraagt naar naam en verwerkt het

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
-import classes
+import sys
+from classes import *
+from PyQt5 import QtWidgets, uic
 
-s = classes.Systeem()
-inputNaam = str(input("Wat is je naam? "))
-afnemer = classes.Afnemer(inputNaam)
-print(afnemer.scoredict)
-for i in s.vragen:
-    print(i)
-    print(s.vragen[i])
-    a = {
-        "a": 0,
-        "b": 1,
-        "c": 2,
-        "d": 3
-    }
-    inputAntwoord = str(input()).lower()
-    iterdict = iter(s.vragen[i])
+def naam():
+    inputNaam = ui.invoernaam.text()
+    return(inputNaam)
 
-    if inputAntwoord in "abcd":
-        val = a[inputAntwoord]
-        print(val)
-        for i in range(0, val):
-            next(iterdict)
-        a = (next(iterdict))
-        print(a)
-        afnemer.addto(a)
-        print(afnemer.scoredict)
-afnemer.schrijf_resultaat()
+def main():
+    s = Systeem()
+    inputNaam = naam()
+    print(f'Je naam is {inputNaam}')
+    afnemer = Afnemer(inputNaam)
+    print(afnemer.scoredict)
+    for i in s.vragen:
+        print(i)
+        print(s.vragen[i])
+        a = {
+            "a": 0,
+            "b": 1,
+            "c": 2,
+            "d": 3
+        }
+        inputAntwoord = str(input()).lower()
+        iterdict = iter(s.vragen[i])
+
+        if inputAntwoord in "abcd":
+            val = a[inputAntwoord]
+            print(val)
+            for i in range(0, val):
+                next(iterdict)
+            a = (next(iterdict))
+            print(a)
+            afnemer.addto(a)
+            print(afnemer.scoredict)
+    afnemer.schrijf_resultaat()
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication([])
+    ui = uic.loadUi("Sorteerhoed.ui")
+    ui.show()
+    app.exec()
+    ui.verzendnaam.clicked.connect(naam)
+    main()
