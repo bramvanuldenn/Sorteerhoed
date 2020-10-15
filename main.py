082010790162faace4cd9b533f9d9d32482c13ec
bramvanuldenn <70747253+bramvanuldenn@users.noreply.github.com>
2020-10-15 22:19:58 +0200

    v0.4
    nieuwe vragen + aanpassingen aan mail voor eigenlijk niks lol maar we zien wel
    
    ik heb pyqt5 changes gediscard aangezien we gekozen hebben om verder te gaan met pygame-gui(?)

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import classes
 
 s = classes.Systeem()
+print("VORIGE RESULTATEN:")
+for i in iter(s.resultaten):
+    r = s.resultaten[i]
+    print(f"Naam:'{r.naam}'")
+    print(f"Datum van afname:{r.datum}")
+    print(f"Scores:{r.scoredict}\n")
 inputNaam = str(input("Wat is je naam? "))
 afnemer = classes.Afnemer(inputNaam)
-print(afnemer.scoredict)
 for i in s.vragen:
     print(i)
     print(s.vragen[i])
@@ -18,11 +23,10 @@ for i in s.vragen:
 
     if inputAntwoord in "abcd":
         val = a[inputAntwoord]
-        print(val)
         for i in range(0, val):
             next(iterdict)
         a = (next(iterdict))
-        print(a)
+        print(f"gekozen antwoord: {a}")
         afnemer.addto(a)
         print(afnemer.scoredict)
 afnemer.schrijf_resultaat()
